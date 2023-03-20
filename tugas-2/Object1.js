ee9c0fbe17a881a6f836219b07c537ecb61988b2
Ghandi-dev <scoutghandi31@gmail.com>
2023-03-20 12:21:10 +0700

    perbaikan

--- tugas-2/Object1.js
+++ tugas-2/Object1.js
@@ -1,21 +1,31 @@
 function arrayToObject(input) {
-    let object = {}
-    let i = 1;
-    const tahunSekarang = new Date().getFullYear()
-    input.forEach(element => {
-        object["firstName"] = element[0]
-        object["lastName"] = element[1]
-        object["gender"] = element[2]
-        object["age"] = (element[3] >= tahunSekarang || typeof (element[3]) == "undefined") ? "Invalid Birth Day" : tahunSekarang - element[3]
-        console.log(`${i++}. ${object.firstName} ${object.lastName} : ${JSON.stringify(object)}`);
-    });
+  let object = {};
+  let i = 1;
+  const tahunSekarang = new Date().getFullYear();
+  input.forEach((element) => {
+    object["firstName"] = element[0];
+    object["lastName"] = element[1];
+    object["gender"] = element[2];
+    object["age"] =
+      element[3] >= tahunSekarang || typeof element[3] == "undefined"
+        ? "Invalid Birth Day"
+        : tahunSekarang - element[3];
+    console.log(
+      `${i++}. ${object.firstName} ${object.lastName} : ${JSON.stringify(
+        object
+      )}`
+    );
+  });
 }
 
-var people = [["Bruce", "Banner", "male", 1975], ["Natasha",
-    "Romanoff", "female"]]
-console.log(typeof (people));
-arrayToObject(people)
-var people2 = [["Tony", "Stark", "male", 1980], ["Pepper", "Pots",
-    "female", 2023]]
-arrayToObject(people2)
-arrayToObject([])
+var people = [
+  ["Bruce", "Banner", "male", 1975],
+  ["Natasha", "Romanoff", "female"],
+];
+arrayToObject(people);
+var people2 = [
+  ["Tony", "Stark", "male", 1980],
+  ["Pepper", "Pots", "female", 2023],
+];
+arrayToObject(people2);
+arrayToObject([]);
--- tugas-2/Object2.js
+++ tugas-2/Object2.js
@@ -1,24 +1,30 @@
 function shoppingTime(memberId, money) {
-    var barang = { 'Sepatu Stacattu': 1500000, 'Baju Zoro': 500000, 'Baju H&N': 250000, 'Sweater Uniklooh': 175000, 'Casing Handphone': 50000 }
-    var purcashed = []
-    var object = { 'memberId': memberId, 'money': money }
-    if (memberId == '' || typeof (memberId) == "undefined") {
-        return "Mohon maaf, toko X hanya berlaku untuk member saja"
-    } else if (money < 50000 || typeof (money) == "undefined") {
-        return "Mohon maaf, uang tidak cukup"
-    } else {
-        Object.keys(barang).forEach(key => {
-            if (money >= barang[key]) {
-                purcashed.push(key)
-                object["listPurcashed"] = purcashed
-                money -= barang[key]
-                object["changeMoney"] = money
-            }
-        })
-        return object
-    }
+  var barang = {
+    "Sepatu Stacattu": 1500000,
+    "Baju Zoro": 500000,
+    "Baju H&N": 250000,
+    "Sweater Uniklooh": 175000,
+    "Casing Handphone": 50000,
+  };
+  var purcashed = [];
+  var object = { memberId: memberId, money: money };
+  if (memberId == "" || typeof memberId == "undefined") {
+    return "Mohon maaf, toko X hanya berlaku untuk member saja";
+  } else if (money < 50000 || typeof money == "undefined") {
+    return "Mohon maaf, uang tidak cukup";
+  } else {
+    Object.keys(barang).forEach((key) => {
+      if (money >= barang[key]) {
+        purcashed.push(key);
+        money -= barang[key];
+        object["listPurcashed"] = purcashed;
+        object["changeMoney"] = money;
+      }
+    });
+    return object;
+  }
 }
-console.log(shoppingTime('82Ku8Ma742', 170000));
-console.log(shoppingTime('', 2475000));
-console.log(shoppingTime('234JdhweRxa53', 15000));
+console.log(shoppingTime("82Ku8Ma742", 170000));
+console.log(shoppingTime("", 2475000));
+console.log(shoppingTime("234JdhweRxa53", 15000));
 console.log(shoppingTime());
